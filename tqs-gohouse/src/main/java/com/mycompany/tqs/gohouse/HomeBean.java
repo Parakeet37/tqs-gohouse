10e6514102d372ef75f8da229efc71c5480c13e4
Joao Serpa <32497693+joaoserpa@users.noreply.github.com>
2018-05-08 16:55:12 +0100

    Added my things

---
+++ tqs-gohouse/src/main/java/com/mycompany/tqs/gohouse/HomeBean.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tqs.gohouse;
+
+/**
+ *
+ * @author demo
+ */
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Singleton;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "homeBean", eager = true)
+@SessionScoped
+@Singleton
+public class HomeBean implements Serializable {
+
+    //Variables
+    protected List<TempPropriadade> propriedades;
+
+    //Constructor
+    public HomeBean() {
+        this.loadProperties();
+    }
+
+    //Has to connect to the database and get all the information from Propriedades
+    private void loadProperties() {
+        this.propriedades = new ArrayList<>();
+        TempPropriadade t;
+        t = new TempPropriadade("Joao", "11", "Desc", 19.8f, "IDK", 0, "https://t-ec.bstatic.com/images/hotel/max1024x768/728/72898182.jpg");
+        propriedades.add(t);
+        TempPropriadade p;
+        p = new TempPropriadade("Joao121", "Titulo 2", "Descrips", 111111.8f, "Too Expensive", 1, "https://t-ec.bstatic.com/images/hotel/max1024x768/728/72898182.jpg");
+        propriedades.add(p);
+
+    }
+
+    public List<TempPropriadade> getPropriedades() {
+        return propriedades;
+    }
+
+    public void setPropriedades(List<TempPropriadade> propriedades) {
+        this.propriedades = propriedades;
+    }
+
+}
