9ef2472a314c5c52a7d23f6227516676e92af0a0
Joao Serpa <32497693+joaoserpa@users.noreply.github.com>
2018-05-19 23:42:05 +0100

    userDetails Complete
    Para ver a página userDetails: http://localhost:8080/tqs-gohouse-1.0-SNAPSHOT/faces/userDetailsPage.xhtml?id=jooo@outlook.com
    
    Como não há ainda users/properties não mostra nada.

--- tqs-gohouse/src/main/java/com/mycompany/tqs/gohouse/HomeBean.java
+++ tqs-gohouse/src/main/java/com/mycompany/tqs/gohouse/HomeBean.java
@@ -2,6 +2,8 @@
 package com.mycompany.tqs.gohouse;
 
 
+import dbClasses.Property;
+import dbClasses.Room;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -22,35 +24,32 @@ import javax.faces.context.FacesContext;
 public class HomeBean implements Serializable {
 
     //Variables
-    protected List<TempPropriadade> propriedades;
-
+    protected List<Room> rooms;
+    
+    //Database handler
+    private final DBHandler dBHandler = new DBHandler();
+    
     //Constructor
     public HomeBean() {
-        this.loadProperties();
+        this.loadRooms();
     }
 
     //Has to connect to the database and get all the information from Propriedades
-    private void loadProperties() {
-        this.propriedades = new ArrayList<>();
-        TempPropriadade t;
-        t = new TempPropriadade("Joao", "11", "Desc", 19.8f, "IDK", 0, "https://t-ec.bstatic.com/images/hotel/max1024x768/728/72898182.jpg");
-        propriedades.add(t);
-        TempPropriadade p;
-        p = new TempPropriadade("Joao121", "Titulo 2", "Descrips", 111111.8f, "Too Expensive", 1, "https://t-ec.bstatic.com/images/hotel/max1024x768/728/72898182.jpg");
-        propriedades.add(p);
-
+    private void loadRooms() {
+        rooms = dBHandler.getAvailableRooms();        
     }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
 
     
-    public List<TempPropriadade> getPropriedades() {
-        return propriedades;
-    }
-
-    public void setPropriedades(List<TempPropriadade> propriedades) {
-        this.propriedades = propriedades;
-    }
-
-    
+ 
     
 }
 
