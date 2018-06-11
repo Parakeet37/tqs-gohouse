2f722f8bc9f0b429216ed03d78832e8963ba441e
HoshiNoDemo <38945670+HoshiNoDemo@users.noreply.github.com>
2018-06-11 12:16:52 +0000

    Update GoHouseRESTUsers.java

--- tqs-gohouse/src/main/java/com/mycompany/tqs/gohouse/GoHouseRESTUsers.java
+++ tqs-gohouse/src/main/java/com/mycompany/tqs/gohouse/GoHouseRESTUsers.java
@@ -10,7 +10,7 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.*;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.annotation.XmlAccessType;
